ae2a270a3cc4d5d8a26c9b1cf05afa0f20941853
Abhinav TV <abhinavtv97@gmail.com>
2021-02-04 07:25:30 +0000

    Initial Commit of a create-react-app

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path');
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+console.log("Listening..");
+// app.listen(3000,"0.0.0.0");
+app.listen(process.env.PORT || 8080);
