999b145f6d6058f685ddfe9258f603ae0083327a
Abhinav TV <abhinavtv97@gmail.com>
2021-02-16 21:55:41 +0530

    Made separate server folder

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
 
 app.get('/', (req, res) => {
   res.send('Hello World!!!!')
 })
-app.listen(port,"0.0.0.0", () => {
+app.listen(port, () => {
   console.log(`Example app listening at ${port}`)
 })
